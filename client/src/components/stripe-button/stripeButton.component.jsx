bca054576758151d82310b2dad97183a32155f0e
Samarth Patel <psamarth8836@gmail.com>
2020-09-14 06:21:08 +0530

    seperating CRA app into client folder, added express server for stripe payments

--- client/src/components/stripe-button/stripeButton.component.jsx
+++ client/src/components/stripe-button/stripeButton.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HR1pjJRC5XKHkMyI4McPnc77vJIXxY0d1ZDXvzjIfoNUz5cEQpVXdNat5T0zNdOTTRlHPy5oG3miMr3n3gTOIDJ00AsdC8ECr';
 
     const onToken = token => {
-        alert('payment Succesful');
-    }
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(response => {
+            alert('Payment Successful');
+        }).catch(err => {
+            console.log('Payment error: ', err);
+            alert('There was an issue with your payment. Please make sure you use a valid credit card');
+        });
+    };
+
     return (
         <StripeCheckout 
             label='Pay Now'
@@ -15,7 +29,7 @@ const StripeCheckoutButton = ({ price }) => {
             billingAddress
             shippingAddress
             image='../../assets/Logo.png'
-            description={`Your total is ${price}`}
+            description={`Your total is Rs.${price}`}
             amount={priceForStripe}
             panelLabel='Pay Now'
             token={onToken}
--- client/src/productlist.js
+++ client/src/productlist.js
@@ -1,7 +1,7 @@
 const productListMen = [
     {
         title: "Wonder Men",
-        price: 900,
+        price: 5,
         description: "Lorem Ipsum",
         imgUrl: ["https://firebasestorage.googleapis.com/v0/b/pinktees-a082b.appspot.com/o/WonderWomen.png?alt=media&token=ff7d9ab1-355b-433b-a0c7-566a874c08ac", "https://firebasestorage.googleapis.com/v0/b/pinktees-a082b.appspot.com/o/BlackPanther.png?alt=media&token=2cf3dc7a-db9e-4de4-a4fc-0c03ee445589"],
         size: ["X", "XL"],
